bfd6591ec9bcfc0fb7bdb98ac9e9cae827fd5c50
Busi, Manasa Deepthi <manasadeepthi.busi@swbc.com>
2024-06-17 19:58:02 -0500

    Add project files.

---
+++ FluentAssertionsWithLoans.Tests/LoanAmountShould.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Loans.Domain.Applications.Values;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    public class LoanAmountShould
+    {
+        [Test]
+        public void StoreCurrencyCode()
+        {
+            //var sut = new LoanAmount("USD", 100_000);
+
+            var loanAmount = new LoanAmount("usd", 100_000);
+
+            //Assert.That(sut.CurrencyCode, Is.EqualTo("USD"));
+            loanAmount.CurrencyCode.Should().Be("USD");
+        }
+    }
+}
