553df418963bcc31ba029004ce52cecce8cce238
Busi, Manasa Deepthi <manasadeepthi.busi@swbc.com>
2024-06-19 11:14:25 -0500

    Adding some more changes for Tests.

--- FluentAssertionsWithLoans.Tests/LoanAmountShould.cs
+++ FluentAssertionsWithLoans.Tests/LoanAmountShould.cs
@@ -11,10 +11,20 @@ namespace Loans.Tests
         {
             //var sut = new LoanAmount("USD", 100_000);
 
-            var loanAmount = new LoanAmount("usd", 100_000);
+            var loanAmount = new LoanAmount("USD", 100_000);
 
             //Assert.That(sut.CurrencyCode, Is.EqualTo("USD"));
             loanAmount.CurrencyCode.Should().Be("USD");
+
+            loanAmount.CurrencyCode.Should().BeEquivalentTo("USD");
+            loanAmount.CurrencyCode.Should().StartWith("U");
+            loanAmount.CurrencyCode.Should().EndWith("D");
+
+            loanAmount.CurrencyCode.Should().BeOneOf("USD", "AUD", "GBP");
+            loanAmount.CurrencyCode.Should().Match("*D");
+            loanAmount.CurrencyCode.Should().Match("*S*");
+            loanAmount.CurrencyCode.Should().MatchRegex("[A-Z]{3}");
+
         }
     }
 }
